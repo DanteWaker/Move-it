bb943acfcbe1a8f7e5e37ffc03aa61285f544b52
JOSE DENNER LIRA DO NASCIMENTO <DanteWaker@users.noreply.github.com>
2021-02-25 00:15:19 -0300

    Dia 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css';
+
 
 
 export default function Home() {
@@ -25,7 +27,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-          
+          <ChallengeBox/>
         </div>
       </section>
     </div>
